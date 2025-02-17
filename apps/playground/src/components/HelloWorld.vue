498ec1e6760818b2d5ff492e2031b93a2373f5b5
Gilmar Andrade <gilmar-andrade@outlook.com>
2025-02-17 18:55:15 -0300

    [playground] using MyBrandButton

--- apps/playground/src/components/HelloWorld.vue
+++ apps/playground/src/components/HelloWorld.vue
@@ -1,16 +1,20 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { MyBrandButton, helloWorld } from '@martelo-vue/core'
 
 defineProps<{ msg: string }>()
 
-const count = ref(0)
+function handleClick() {
+  helloWorld()
+}
+
+helloWorld()
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <MyBrandButton @click="handleClick">My precious button</MyBrandButton>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
