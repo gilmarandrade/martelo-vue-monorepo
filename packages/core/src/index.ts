1f8aca5f3f9bea141df77d77fa1c0bd73d0bae2d
Gilmar Andrade <gilmar-andrade@outlook.com>
2025-02-17 17:39:35 -0300

    [core] configure vite build lib mode to support vue and generate .d.ts files

---
+++ packages/core/src/index.ts
@@ -0,0 +1,9 @@
+export function testFunction() {
+    console.log('Hello World from @martelo-vue/core')
+}
+
+export default class TestClass {
+    constructor() {
+
+    }
+}
--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  build: {
+    lib: {
+      entry: "src/index.ts",
+      formats: ["es"],
+      name: "MarteloVue Core",
+      fileName: 'martelo-vue-core'
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          Vue: "vue"
+        }
+      }
+    }
+  },
+  plugins: [
+    vue(), 
+    dts({
+      insertTypesEntry: true
+    })
+  ],
 })
