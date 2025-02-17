2e460caefbe629d7dfedf892ea1683bee04ddf6a
Gilmar Andrade <gilmar-andrade@outlook.com>
2025-02-17 18:54:56 -0300

    [core] refactor MyBrandButton

--- packages/core/src/MyBrandButton/MyBrandButton.vue
+++ packages/core/src/MyBrandButton/MyBrandButton.vue
@@ -1,14 +1,31 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { decrement } from '..';
+
+const count = ref(0)
+
+function handleClick() {
+    count.value = decrement(count.value)
+    console.log('MyBrandButton click', count.value)
+}
+</script>
 <template>
-    <button>
+    <button class="my-brand-button" @click="handleClick">
         <slot></slot>
+        <span>: {{ count }} clicks</span>
     </button>
 </template>
 
 <style scoped>
-button {
+.my-brand-button {
     padding: 20px;
     border-radius: 8px;
-    background-color: cornflowerblue;
+    background-color: purple;
+    color: white;
     border: none;
+}
+
+.my-brand-button:hover {
+    background-color: rgb(255, 0, 255);
 }
 </style>
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,11 @@
 import "./style.css"
 
 export { default as MyBrandButton } from "./MyBrandButton/MyBrandButton.vue"
+
+export function decrement(count: number) {
+    return count - 1
+}
+
+export function helloWorld() {
+    console.log('@martelo-vue/core hello world!')
+}
