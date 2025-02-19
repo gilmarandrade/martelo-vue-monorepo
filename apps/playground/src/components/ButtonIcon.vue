8cff44e5bc2308e07064254de2b21962359dcd42
Gilmar Andrade <gilmar-andrade@outlook.com>
2025-02-19 07:43:32 -0300

    [playground] ButtonIcon style

---
+++ apps/playground/src/components/ButtonIcon.vue
@@ -0,0 +1,26 @@
+<template>
+    <button class="button button--icon">
+        <slot></slot>
+    </button>
+</template>
+<style scoped>
+.button {
+    border: none;
+    background: none;
+    padding: 4px 8px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 4px;
+    cursor: pointer;
+}
+
+.button:hover {
+    background-color: rgba(0, 0, 0, 0.1);
+}
+
+.button--icon {
+    width: 24px;
+    height: 24px;
+}
+</style>
