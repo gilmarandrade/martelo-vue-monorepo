158a70c10a371de4e3b4bbdc52fc33b9f363b497
Gilmar Andrade <gilmar-andrade@outlook.com>
2025-02-19 07:53:50 -0300

    [playground] Badge style

---
+++ apps/playground/src/components/Badge.vue
@@ -0,0 +1,18 @@
+<template>
+    <span class="badge">
+        <slot></slot>
+    </span>
+</template>
+<style scoped>
+.badge {
+    background-color: rgb(0, 162, 255);
+    padding: 4px 8px;
+    border-radius: 8px;
+    color: white;
+    font-size: .8em;
+    line-height: .8em;
+    min-width: 22px;
+    display: inline-block;
+    text-align: center;
+}
+</style>
