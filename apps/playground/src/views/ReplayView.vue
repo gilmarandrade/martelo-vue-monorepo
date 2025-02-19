371ea6ce6ba423e85ab2dbee66252c144b0f3fb4
Gilmar Andrade <gilmar-andrade@outlook.com>
2025-02-19 08:01:37 -0300

    [playground] ReplayView grid layout

--- apps/playground/src/views/ReplayView.vue
+++ apps/playground/src/views/ReplayView.vue
@@ -1,3 +1,32 @@
 <template>
-    replay view
+    <div class="replay-layout">
+        <aside class="aside">
+            aside
+        </aside>
+        <main class="main">
+            main
+            replay view
+        </main>
+    </div>
 </template>
+<script setup lang="ts">
+
+</script>
+<style scoped>
+.replay-layout {
+    background-color: violet;
+    width: 100vw;
+    height: 100vh;
+    display: grid;
+    grid-template-columns: 300px auto;
+}
+
+.replay-layout .aside {
+    background-color: yellow;
+}
+
+.replay-layout .main {
+    background-color: springgreen;
+}
+
+</style>
