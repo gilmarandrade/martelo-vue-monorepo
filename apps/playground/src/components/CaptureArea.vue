6012163553997059fe756a69d4fd66627832884c
Gilmar Andrade <gilmar-andrade@outlook.com>
2025-02-19 07:33:13 -0300

    [playground] CaptureArea style

---
+++ apps/playground/src/components/CaptureArea.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="capture-area">
+    </div>
+</template>
+
+<style scoped>
+.capture-area {
+    border: 1px solid rgb(179, 179, 179);
+    box-shadow: 0px 0px 7px -2px rgba(0,0,0,0.75);
+    width: 800px;
+    height: 500px;
+}
+</style>
--- apps/playground/src/views/CaptureView.vue
+++ apps/playground/src/views/CaptureView.vue
@@ -1,3 +1,9 @@
 <template>
     capture view
+    <CaptureArea/>
 </template>
+
+<script setup lang="ts">
+import CaptureArea from '../components/CaptureArea.vue';
+
+</script>
